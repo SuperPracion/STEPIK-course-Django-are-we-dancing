e36143a68d097e57b620c1d88485d49b62c9a19d
SuperPracion <pracion@bk.ru>
2023-04-04 17:51:50 +0300

    Выполнение модуля 3.3

--- my_site/blog/urls.py
+++ my_site/blog/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.posts),
+    path('', views.get_guinness_world_records),
     path('<int:number>/', views.get_info_about_number),
     path('<str:word>/', views.get_info_about_word),
 ]
--- my_site/blog/views.py
+++ my_site/blog/views.py
@@ -4,13 +4,35 @@ from django.http import HttpResponse
 
 # Create your views here.
 def get_info_about_word(request, word: str):
-    return HttpResponse(f'Это страница {word}')
+    data = {
+        'name': word
+    }
+    return render(request, 'blog/detail_by_name.html', context=data)
+    #return HttpResponse(f'Это страница {word}')
 
 
 def get_info_about_number(reqiest, number: int):
-    return HttpResponse(f'Здесь содержится информация о посте под номером {number}')
+    data = {
+        'number': number
+    }
+    return render(reqiest, 'blog/detail_by_number.html', context=data)
+    #return HttpResponse(f'Здесь содержится информация о посте под номером {number}')
 
 
 def posts(request):
-    return render(request, 'blog/list_detail.html')
-    #return HttpResponse(f'Это страница posts')
+    # data = {
+    #     'year_born': 'CONTENT',
+    #     'city_born': 'CONTENT',
+    #     'movie_name': 'CONTENT'
+    # }
+    # return render(request, 'blog/test.html', context=data)
+    return HttpResponse(f'Это страница posts')
+
+
+def get_guinness_world_records(request):
+    context = {
+        'power_man': 'Narve Laeret',
+        'bar_name': 'Bob’s BBQ & Grill',
+        'count_needle': 1790,
+    }
+    return render(request, 'blog/guinnessworldrecords.html', context=context)
